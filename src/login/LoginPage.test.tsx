a0076120d70ef2e61f16d6265db1f8b170205c93
Eric Horodyski <horodyski@ionic.io>
2023-04-12 12:27:32 -0400

    chore: bump up @ionic/react to fix unit testing

--- src/login/LoginPage.test.tsx
+++ src/login/LoginPage.test.tsx
@@ -1,7 +1,10 @@
+import { vi } from 'vitest';
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import LoginPage from './LoginPage';
 
 describe('<LoginPage />', () => {
+  beforeEach(() => vi.clearAllMocks());
+
   it('renders consistently', () => {
     const { asFragment } = render(<LoginPage />);
     expect(asFragment()).toMatchSnapshot();
@@ -15,34 +18,48 @@ describe('<LoginPage />', () => {
 
   describe('error messages', () => {
     it('displays an error if the e-mail address is dirty and empty', async () => {
-      // render(<LoginPage />);
-      // const email = await waitFor(() => screen.getByLabelText('Email Address'));
-      // await waitFor(() => fireEvent.change(email, { target: { value: '' } }));
-      // await waitFor(() => fireEvent.blur(email));
-      // console.log(email);
-      //expect(email).toHaveTextContent(/Email address is a required field/);
+      render(<LoginPage />);
+      const email = await waitFor(() => screen.getByLabelText('Email Address'));
+      await waitFor(() => fireEvent.input(email, { target: { value: 'test@test.com' } }));
+      await waitFor(() => fireEvent.blur(email));
+      await waitFor(() => fireEvent.input(email, { target: { value: '' } }));
+      await waitFor(() => expect(screen.getByText(/Email address is a required field/)).toBeInTheDocument());
     });
 
-    it('displays an error if the email address has an invalid format', async () => {});
+    it('displays an error if the email address has an invalid format', async () => {
+      render(<LoginPage />);
+      const email = await waitFor(() => screen.getByLabelText('Email Address'));
+      await waitFor(() => fireEvent.input(email, { target: { value: 'test' } }));
+      await waitFor(() => fireEvent.blur(email));
+      await waitFor(() => expect(screen.getByText(/Email address must be a valid email/)).toBeInTheDocument());
+    });
 
-    it('displays an error message if the password is dirty and empty', async () => {});
+    it('displays an error message if the password is dirty and empty', async () => {
+      render(<LoginPage />);
+      const password = await waitFor(() => screen.getByLabelText('Password'));
+      await waitFor(() => fireEvent.input(password, { target: { value: '' } }));
+      await waitFor(() => fireEvent.blur(password));
+      await waitFor(() => expect(screen.getByText(/Password is a required field/)).toBeInTheDocument());
+    });
   });
 
   describe('sign in button', () => {
-    it('starts disabled', () => {});
+    it('starts disabled', async () => {
+      render(<LoginPage />);
+      const button = await waitFor(() => screen.getByText('Sign In') as HTMLIonButtonElement);
+      expect(button.disabled).toBeTruthy();
+    });
 
-    it('is disabled with just an email address', () => {});
-
-    it('is disabled with just a password', () => {});
-
-    it('is enabled with both an email address and password', async () => {
-      // render(<LoginPage />);
-      // const button = (await waitFor(() => screen.getByText('Sign In'))) as HTMLIonButtonElement;
-      // const email = await waitFor(() => screen.getByLabelText('Email Address'));
-      // const password = await waitFor(() => screen.getByLabelText('Password'));
-      // await waitFor(() => fireEvent.change(password, { target: { value: 'test@ionic.io' } }));
-      // await waitFor(() => fireEvent.change(email, { target: { value: 'test@ionic.io' } }));
-      // expect(button.disabled).toBeFalsy();
+    it('is enabled once valid data is entered', async () => {
+      render(<LoginPage />);
+      const button = await waitFor(() => screen.getByText('Sign In') as HTMLIonButtonElement);
+      const password = await waitFor(() => screen.getByLabelText('Password'));
+      const email = await waitFor(() => screen.getByLabelText('Email Address'));
+      expect(button.disabled).toBeTruthy();
+      await waitFor(() => fireEvent.input(email, { target: { value: 'test@test.com' } }));
+      expect(button.disabled).toBeTruthy();
+      await waitFor(() => fireEvent.input(password, { target: { value: 'password' } }));
+      expect(button.disabled).toBeFalsy();
     });
   });
 });
