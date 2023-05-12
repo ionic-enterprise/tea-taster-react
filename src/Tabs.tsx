16c80c364b2f7a6deaef6646ec9c1741452c010e
Eric Horodyski <horodyski@ionic.io>
2023-05-12 08:48:19 -0400

    fix(step 13): remove <IonContent> from Tabs.tsx

--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -10,42 +10,40 @@ const Tabs: React.FC = () => {
   const { url } = useRouteMatch();
 
   return (
-    <IonContent>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route exact path={`${url}/about`}>
-            <AboutPage />
-          </Route>
-          <Route exact path={`${url}/tasting-notes`}>
-            <TastingNotesPage />
-          </Route>
-          <Route exact path={`${url}/tea`}>
-            <TeaListPage />
-          </Route>
-          <Route exact path={`${url}/tea/:id`}>
-            <TeaDetailsPage />
-          </Route>
-          <Route exact path={url}>
-            <Redirect to={`${url}/tea`} />
-          </Route>
-        </IonRouterOutlet>
+    <IonTabs>
+      <IonRouterOutlet>
+        <Route exact path={`${url}/about`}>
+          <AboutPage />
+        </Route>
+        <Route exact path={`${url}/tasting-notes`}>
+          <TastingNotesPage />
+        </Route>
+        <Route exact path={`${url}/tea`}>
+          <TeaListPage />
+        </Route>
+        <Route exact path={`${url}/tea/:id`}>
+          <TeaDetailsPage />
+        </Route>
+        <Route exact path={url}>
+          <Redirect to={`${url}/tea`} />
+        </Route>
+      </IonRouterOutlet>
 
-        <IonTabBar color="tertiary" slot="bottom">
-          <IonTabButton tab="tea" href={`${url}/tea`}>
-            <IonIcon icon={leaf} />
-            <IonLabel>Tea</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tasting-notes" href={`${url}/tasting-notes`}>
-            <IonIcon icon={documentText} />
-            <IonLabel>Tasting Notes</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="about" href={`${url}/about`}>
-            <IonIcon icon={informationCircle} />
-            <IonLabel>About</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonContent>
+      <IonTabBar color="tertiary" slot="bottom">
+        <IonTabButton tab="tea" href={`${url}/tea`}>
+          <IonIcon icon={leaf} />
+          <IonLabel>Tea</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="tasting-notes" href={`${url}/tasting-notes`}>
+          <IonIcon icon={documentText} />
+          <IonLabel>Tasting Notes</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="about" href={`${url}/about`}>
+          <IonIcon icon={informationCircle} />
+          <IonLabel>About</IonLabel>
+        </IonTabButton>
+      </IonTabBar>
+    </IonTabs>
   );
 };
 export default Tabs;
